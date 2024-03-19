be3a3e6a77038b41313a2901046df3937ceebea8
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-19 16:35:53 -0700

    Refactor detail with shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
+# manual render method:
 # def index(request):
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
 #     template = loader.get_template("polls/index.html")
@@ -11,13 +12,24 @@ from .models import Question
 #     }
 #     return HttpResponse(template.render(context, request))
 
+# shortcut render method:
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 
+# manual 404 method: 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, "polls/detail.html", {"question": question})
+
+# shortcut 404 method:
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You're looking at the results of %s"
