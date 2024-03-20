8c4a559a3ea2cf6f76678f74b6e6136ad42fc720
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-20 11:01:28 -0700

    Refactor urlConfig and viewsConfig to use generic views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,39 +1,35 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-# from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
-# manual render method:
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
 # def index(request):
 #     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {
-#       "latest_question_list": latest_question_list
-#     }
-#     return HttpResponse(template.render(context, request))
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "polls/index.html", context)
 
-# shortcut render method:
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-# manual 404 method: 
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
+# def detail(request, question_id):     (and results)
+#     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, "polls/detail.html", {"question": question})
 
-# shortcut 404 method:
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -52,9 +48,3 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
-
