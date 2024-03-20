b9cd24aa57cccb179adc9690351d9335efbdd163
mejia-dev <aaron.mejia8484@gmail.com>
2024-03-20 10:29:39 -0700

    Redefined views for vote and results for form handling

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.template import loader
 from django.shortcuts import get_object_or_404, render
-from .models import Question
+from django.urls import reverse
+
+from .models import Choice, Question
 
 # manual render method:
 # def index(request):
@@ -31,9 +34,27 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
-def results(request, question_id):
-    response = "You're looking at the results of %s"
-    return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
